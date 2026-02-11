de8100ec05aff22f036df15b0df0a90d28e64251
Juan Felipe Rojas <58739212+juanFeRoz@users.noreply.github.com>
2026-02-11 18:58:21 -0500

    Updated pom file

--- src/main/java/co/edu/demoacademico/DemoAcademicoApplication.java
+++ src/main/java/co/edu/demoacademico/DemoAcademicoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoAcademicoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoAcademicoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAcademicoApplication.class, args);
+    }
 
 }
