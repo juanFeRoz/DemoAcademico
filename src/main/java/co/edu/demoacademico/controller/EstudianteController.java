e59f39259b189aa6d3ddafd95e5e23673eb2235a
davidserrano0801 <davidserrano0801@gmail.com>
2026-02-13 19:12:32 -0500

    added swager ui

--- src/main/java/co/edu/demoacademico/controller/EstudianteController.java
+++ src/main/java/co/edu/demoacademico/controller/EstudianteController.java
@@ -27,6 +27,7 @@ public class EstudianteController {
     public List<Estudiante> listar() {
         return service.listar();
     }
+    @GetMapping
     @RequestMapping(value="/buscar", params="email")
     public Optional<Estudiante> buscar(@RequestParam String email){
         return service.buscar(email);
